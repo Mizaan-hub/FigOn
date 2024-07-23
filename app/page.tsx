0378b71ce31e0d0efac69daf85adf46a27b845c3
Mizaan-hub <mizaanafandi@gmail.com>
2024-07-24 00:11:50 +0530

    removed room tag from page and added to layouts, and customized font and bg color

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import { Room } from "./Room";
 
 export default function Page() {
   return (
-    <Room>
-      <h1 className="font-extralight">Fig On</h1>
-    </Room>
+      <h1 className="text-5xl">Fig On</h1>
   );
 }
